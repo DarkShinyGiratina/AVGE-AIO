86a7b20b769ddb08f80f034917fb4185f3e68baa
DarkShinyGiratina <ross_williams@brown.edu>
2024-10-06 10:24:14 -0400

    slight edit to the ffmpeg args

--- Tools/ffmpeg_encode.py
+++ Tools/ffmpeg_encode.py
@@ -4,7 +4,7 @@ import subprocess
 ffmpeg_path = shutil.which("ffmpeg", path="./ffmpeg")
 ffprobe_path = shutil.which("ffprobe", path="./ffmpeg")
 ffprobe_args = "-v error -select_streams v:0 -count_packets -show_entries stream=nb_read_packets -of csv=p=0".split()
-ffmpeg_args = "-y -v quiet -stats -i ytdloutput.webm -c:v libx264 -crf 18 -c:a aac".split()
+ffmpeg_args = "-y -v quiet -stats -i input_name -c:v libx264 -crf 18 -c:a aac".split()
 progress = 0.0
 
 
@@ -18,6 +18,7 @@ def convert_to_mp4(file: str, output_name: str) -> None:
 
     if ffmpeg_args[0] != ffmpeg_path:
         ffmpeg_args.insert(0, ffmpeg_path)
+    ffmpeg_args[6] = file
     ffmpeg_args.append(output_name)
     ffmpeg_proc = subprocess.Popen(ffmpeg_args, stdout=subprocess.PIPE,
                                    stderr=subprocess.STDOUT, text=True)
